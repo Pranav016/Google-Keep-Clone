41e96b9807b9f836419708b67638d2110246a2a2
Pranav <54665036+Pranav016@users.noreply.github.com>
2021-01-07 17:02:33 +0530

    Updated index.html and styles.css and added createArea component

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = React.useState({ title: "", content: "" });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  function addNote(event) {
+    props.onAdd(note);
+    setNote({ title: "", content: "" });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          placeholder="Title"
+          value={note.title}
+          onChange={handleChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+          onChange={handleChange}
+        />
+        <button onClick={addNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
